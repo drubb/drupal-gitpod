3b498aa0a35aaec0d458211b4eec56215231088d
drubb <info@drubb.de>
2020-10-11 11:41:12 +0200

    Improved instruction handling

--- command.sh
+++ command.sh
@@ -2,7 +2,7 @@
 set -e
 
 clear
-gp open "instructions-${DRUPAL_MAJOR:-9}.md"
+cp "instructions-${DRUPAL_MAJOR:-9}.md" README.md
 sleep 1
 if [ "$DRUPAL_MAJOR" = "none" ]
 then
@@ -11,7 +11,8 @@ then
   exit 0
 fi
 [ -d "./drupal" ] && cd drupal
-echo 'Your Drupal instance is being prepared. You can watch the progress in the second terminal window ("Start & Run")'.
+echo 'Your Drupal instance is being prepared, this will take some time.'.
+echo 'You can watch the progress in the second terminal window ("Start & Run")'.
 gp await-port 8888
-printf "\nYour Drupal instance is ready. Please open the server port using the popup in the lower right corner\nor use this Drush link to login immediately (Cmd/Ctrl+Click):\n"
+printf "\nYour Drupal instance is ready. Please open the server port using the popup in the lower right corner\nor use this Drush link to login immediately (Cmd/Ctrl+Click):\n\n"
 drush uli
