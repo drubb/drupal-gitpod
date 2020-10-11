ae6ce15cefea50780718e80aa049a6e2369a4b60
drubb <info@drubb.de>
2020-10-11 11:13:10 +0200

    Some text fixes

--- command.sh
+++ command.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 set -e
 
+clear
 gp open "instructions-${DRUPAL_MAJOR:-9}.md"
 sleep 1
 if [ "$DRUPAL_MAJOR" = "none" ]
 then
   clear
-  printf "It's up to you! Spin up your custom Drupal instance using Composer and Drush. Example:\n\ncomposer create-project <template> somedir\ncd somedir\ncomposer require drush/drush\ndrush si\ndrush serve 0.0.0.0:8888\n"
+  printf "It's up to you! Spin up your custom Drupal instance using Composer and Drush. See above for an example on how to do this."
   exit 0
 fi
 [ -d "./drupal" ] && cd drupal
 echo 'Your Drupal instance is being prepared. You can watch the progress in the second terminal window ("Start & Run")'.
 gp await-port 8888
-echo 'Your Drupal instance is ready. Pleas, open the server port using the popup to to the right or use this Drush link to login immediately (Cmd/Ctrl+Click):'
+printf "\nYour Drupal instance is ready. Please open the server port using the popup in the lower right corner\nor use this Drush link to login immediately (Cmd/Ctrl+Click):\n"
 drush uli
